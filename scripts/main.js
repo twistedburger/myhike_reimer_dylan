56024d9c64cc588a5ad99a0741a99847b9bd7c78
Dylan Reimer <reimer.dylan@gmail.com>
2024-03-19 10:19:01 -0700

    Complete demo 10

--- scripts/main.js
+++ scripts/main.js
@@ -62,6 +62,7 @@ function displayCardsDynamically(collection) {
                 var details = doc.data().details;  // get value of the "details" key
                 var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
                 var hikeLength = doc.data().length; //gets the length field
+                var docID = doc.id;
                 let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
 
                 //update title and text and image
@@ -69,6 +70,7 @@ function displayCardsDynamically(collection) {
                 newcard.querySelector('.card-length').innerHTML = hikeLength + "km";
                 newcard.querySelector('.card-text').innerHTML = details;
                 newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+                newcard.querySelector('a').href = "eachHike.html?docID=" + docID;
 
                 //Optional: give unique ids to all elements for future use
                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
